728e6875c88ff60cae5de4f548497b1b41d00145
Nikolas <timofeevnik41@gmail.com>
2022-08-01 20:39:42 +0300

    :zap: Encode employee on creation

--- modules/routers/employees/router.py
+++ modules/routers/employees/router.py
@@ -4,7 +4,7 @@ from fastapi import APIRouter, Depends
 
 from ...database import MongoDbWrapper
 from ...dependencies.security import check_user_permissions, get_current_user
-from ...exceptions import DatabaseException
+from ...exceptions import DatabaseException, NotFoundException
 from .models import Employee, EmployeeOut, EmployeesOut, EncodedEmployee, GenericResponse
 
 router = APIRouter(dependencies=[Depends(get_current_user)])
@@ -24,6 +24,7 @@ async def get_all_employees(page: int = 1, items: int = 20) -> EmployeesOut:
 async def create_new_employee(employee_data: Employee) -> GenericResponse:
     """Endpoint to create new employee"""
     try:
+        employee_data.passport_code = await employee_data.encode_sha256()
         await MongoDbWrapper().add_employee(employee_data)
     except Exception as exception_message:
         raise DatabaseException(error=exception_message)
@@ -48,7 +49,7 @@ async def get_employee_by_card_id(rfid_card_id: str) -> tp.Union[EmployeeOut, Ge
     except Exception as exception_message:
         raise DatabaseException(error=exception_message)
     if employee is None:
-        return GenericResponse(status_code=404, detail="Not found")
+        raise NotFoundException(detail=f"Employee with id {rfid_card_id} not found")
     return EmployeeOut(employee=employee)
 
 
@@ -68,10 +69,8 @@ async def decode_existing_employee(encoded_employee: EncodedEmployee) -> Employe
     try:
         employee = await MongoDbWrapper().get_concrete_employee(passport_code=encoded_employee.encoded_name)
         if not employee:
-            return EmployeeOut(
-                status_code=404,
-                detail=f"Can't decode employee with code {encoded_employee.encoded_name}, no one found",
-                employee=None,
+            raise NotFoundException(
+                detail=f"Employee with code {encoded_employee.encoded_name[:3]}...{encoded_employee.encoded_name[-3:]} not found",
             )
     except Exception as exception_message:
         raise DatabaseException(error=exception_message)
