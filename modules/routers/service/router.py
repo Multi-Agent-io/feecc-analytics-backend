ec61dedb0ff4395060209d00983b1daa17349e72
Nikolas <timofeevnik41@gmail.com>
2022-11-10 09:44:54 +0300

    keep session alive for 1024 hrs

--- modules/routers/service/router.py
+++ modules/routers/service/router.py
@@ -28,7 +28,7 @@ async def get_server_status() -> dict[str, str]:
 
 
 @router.post("/token", response_model=Token)
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()) -> dict[str, str]:
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), keep_session: bool = True) -> dict[str, str]:
     """
     Endpoint for user-auth
 
@@ -39,6 +39,8 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         logger.warning(f"Failed to login user {form_data.username}")
         raise AuthException
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    if keep_session:
+        access_token_expires = timedelta(hours=1024)
     access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
     return {"access_token": access_token, "token_type": "bearer"}
 
